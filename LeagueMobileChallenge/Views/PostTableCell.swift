5884a57dd14bf08548772a4731d36771ade66b36
banie <banie.setijoso@gmail.com>
2023-03-31 18:01:28 -0400

    Add the UIImageView to the view to show avatar and use the provided SDWebImage to show the image through the url

--- LeagueMobileChallenge/Views/PostTableCell.swift
+++ LeagueMobileChallenge/Views/PostTableCell.swift
@@ -8,19 +8,24 @@
 
 import Foundation
 import UIKit
+import SDWebImage
 
 class PostTableCell: UITableViewCell {
+    @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descLabel: UILabel!
     
     override func prepareForReuse() {
+        super.prepareForReuse()
+        
         usernameLabel.text = nil
         titleLabel.text = nil
         descLabel.text = nil
     }
     
     func decorate(with postItem: PostItem) {
+        avatarImageView.sd_setImage(with: postItem.avatar, placeholderImage: UIImage(systemName: "pencil.circle"))
         usernameLabel.text = postItem.userName
         titleLabel.text = postItem.title
         descLabel.text = postItem.body
