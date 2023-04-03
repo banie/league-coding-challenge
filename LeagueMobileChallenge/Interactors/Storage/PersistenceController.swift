5d7236c042613b9c0bc10e26ab18cfbc80a390d9
banie <banie.setijoso@gmail.com>
2023-04-03 14:03:38 -0400

    Create PersistenceController

---
+++ LeagueMobileChallenge/Interactors/Storage/PersistenceController.swift
@@ -0,0 +1,36 @@
+//
+//  PersistenceController.swift
+//  LeagueMobileChallenge
+//
+//  Created by banie setijoso on 2023-04-03.
+//  Copyright © 2023 Banie Setijoso. All rights reserved.
+//
+
+import Foundation
+
+import CoreData
+
+class PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+    let backgroundContext: NSManagedObjectContext
+    
+    var viewContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "LeagueMobileChallenge")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext = container.newBackgroundContext()
+    }
+}
